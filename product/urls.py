4ed69fc157878e5f8da951df6e8393e163f1bcaf
sergA0312 <117915149+sergA0312@users.noreply.github.com>
2023-10-26 15:22:49 +0600

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path('categories/create/', CategoryCreate.as_view(), name='category-create'),
     path('categories/update/<int:pk>/', CategoryDetailUpdateDelete.as_view(), name='category-detail-update-delete'),
     path('products/create/', ProductCreate.as_view(), name='product-create'),
-    path('products/update/<int:pk>/', ProductDetailUpdateDelete.as_view(), name='product-detail-update-delete')
+    path('products/update/<int:pk>/', ProductDetailUpdateDelete.as_view(), name='product-detail-update-delete'),
     path('reviews/create/', ReviewCreate.as_view(), name='review-create'),
     path('reviews/update/<int:pk>/', ReviewDetailUpdateDelete.as_view(), name='review-detail-update-delete'),
 ]
